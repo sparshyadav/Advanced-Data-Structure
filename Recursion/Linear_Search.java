cb39ea66d0c99eafaeedee4e99e609f7e747a3f0
Sparsh Yadav <96286051+sparshyadav@users.noreply.github.com>
2024-02-13 20:24:14 +0530

    Linear Search Using Recursion

--- Recursion/Linear_Search.java
+++ Recursion/Linear_Search.java
@@ -1,3 +1,22 @@
 public class Linear_Search {
-    
+    static int linearSearch(int[] arr, int target, int index) {
+        if (index >= arr.length) {
+            return -1;
+        }
+
+        if (arr[index] == target) {
+            return index;
+        }
+
+        return linearSearch(arr, target, index + 1);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 5, 3, 7, 2, 9 };
+        int target = 7;
+
+        int ans = linearSearch(arr, target, 0);
+
+        System.out.println(ans);
+    }
 }
